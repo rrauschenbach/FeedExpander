25d87bbbd0f10d76d38f22519c70a5e2ca2de10a
meerkatzenwildschein <guru@freya>
2015-12-25 19:56:04 +0100

    use static imports for Assert

--- src/test/java/org/rr/expander/feed/ExpressionParserTest.java
+++ src/test/java/org/rr/expander/feed/ExpressionParserTest.java
@@ -1,8 +1,9 @@
 package org.rr.expander.feed;
 
+import static org.junit.Assert.*;
+
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.rr.expander.feed.ExpressionParser;
 
@@ -11,51 +12,51 @@ public class ExpressionParserTest {
 	@Test
 	public void testZeroExpressionCount() {
 		List<ExpressionParser> parsers = ExpressionParser.createExpressionParser("");
-		Assert.assertEquals(0, parsers.size());
+		assertEquals(0, parsers.size());
 	}
 	
 	@Test
 	public void testNullExpression() {
 		List<ExpressionParser> parsers = ExpressionParser.createExpressionParser(null);
-		Assert.assertEquals(0, parsers.size());
+		assertEquals(0, parsers.size());
 	}
 	
 	@Test
 	public void testOneExpressionCount() {
 		List<ExpressionParser> parsers = ExpressionParser.createExpressionParser("tag=div");
-		Assert.assertEquals(1, parsers.size());
+		assertEquals(1, parsers.size());
 		ExpressionParser parser = parsers.get(0);
 		
-		Assert.assertEquals(1, parser.segmentCount());
+		assertEquals(1, parser.segmentCount());
 	}
 	
 	@Test
 	public void testMultipleExpressionCount() {
 		List<ExpressionParser> parsers = ExpressionParser.createExpressionParser("tag=div/tag=div/tag=div");
-		Assert.assertEquals(1, parsers.size());
+		assertEquals(1, parsers.size());
 		ExpressionParser parser = parsers.get(0);
 		
-		Assert.assertEquals(3, parser.segmentCount());
+		assertEquals(3, parser.segmentCount());
 	}
 	
 	@Test
 	public void testExpressionGetTag() {
 		List<ExpressionParser> parsers = ExpressionParser.createExpressionParser("tag=div/tag=p");
-		Assert.assertEquals(1, parsers.size());
+		assertEquals(1, parsers.size());
 		ExpressionParser parser = parsers.get(0);
 		
-		Assert.assertEquals("div", parser.getSegmentValue(0));
-		Assert.assertEquals("p", parser.getSegmentValue(1));
+		assertEquals("div", parser.getSegmentValue(0));
+		assertEquals("p", parser.getSegmentValue(1));
 	}
 	
 	@Test
 	public void testExpressionGetId() {
 		List<ExpressionParser> parsers = ExpressionParser.createExpressionParser("id=test1/id=test2");
-		Assert.assertEquals(1, parsers.size());
+		assertEquals(1, parsers.size());
 		ExpressionParser parser = parsers.get(0);
 		
-		Assert.assertEquals("test1", parser.getSegmentValue(0));
-		Assert.assertEquals("test2", parser.getSegmentValue(1));
+		assertEquals("test1", parser.getSegmentValue(0));
+		assertEquals("test2", parser.getSegmentValue(1));
 	}
 	
 }
